e69af309b36d18754e90b2ebaec713f690bddf05
Loic <loic.weiss@epitech.eu>
2019-04-25 15:35:46 +0200

    norm

--- norm/src/malloc.c
+++ norm/src/malloc.c
@@ -12,7 +12,7 @@ void malloc_tetrimino_struct(tetris_t *tetris)
         if (tetris->tetrimino[i].error_detected == 0) {
             tetris->tetrimino[i].form_tetrimino = malloc(sizeof(char *) * tetris->tetrimino[i].height);
             for (int k = 0; k != tetris->tetrimino[i].height; k++) {
-                tetris->tetrimino[i].form_tetrimino[k] = malloc(sizeof(char) * tetris->tetrimino[i].width);
+                tetris->tetrimino[i].form_tetrimino[k] = malloc(sizeof(char) * tetris->tetrimino[i].width + 1);
             }
         }
     }
@@ -22,9 +22,9 @@ void malloc_tetrimino_without_space(tetris_t *tetris)
 {
     for (int i = 0; i != tetris->get_number_tetrimino; i++) {
         if (tetris->tetrimino[i].error_detected == 0) {
-            tetris->tetrimino[i].tetri_without_final_space = malloc(sizeof(char *) * tetris->tetrimino[i].height);
+            tetris->tetrimino[i].tetri_without_final_space = malloc(sizeof(char *) * tetris->tetrimino[i].height + 1);
             for (int k = 0; k != tetris->tetrimino[i].height; k++) {
-                tetris->tetrimino[i].tetri_without_final_space[k] = malloc(sizeof(char) * tetris->tetrimino[i].width);
+                tetris->tetrimino[i].tetri_without_final_space[k] = malloc(sizeof(char) * tetris->tetrimino[i].width + 1);
             }
         }
     }
@@ -41,11 +41,11 @@ int malloc_file_array(tetris_t *tetris)
     tetris->strcat_register_tetrimino_file = malloc(sizeof(char *) * tetris->get_number_tetrimino);
 
     for (i; i != tetris->get_number_tetrimino; i++)
-        tetris->register_tetrimino_file[i] = malloc(sizeof(char) * tetris->get_max_size);
+        tetris->register_tetrimino_file[i] = malloc(sizeof(char) * tetris->get_max_size + 1);
 
     for (j; j != tetris->get_number_tetrimino; j++)
-        tetris->name_tetrimino[j] = malloc(sizeof(char) * (tetris->get_max_size - my_strlen(".tetrimino")));
+        tetris->name_tetrimino[j] = malloc(sizeof(char) * (tetris->get_max_size - my_strlen(".tetrimino")) + 1);
 
     for (k; k != tetris->get_number_tetrimino; k++)
-        tetris->strcat_register_tetrimino_file[k] = malloc(sizeof(char) * (tetris->get_max_size + my_strlen("tetrimino/")));
+        tetris->strcat_register_tetrimino_file[k] = malloc(sizeof(char) * (tetris->get_max_size + my_strlen("tetrimino/") + 1));
 }
